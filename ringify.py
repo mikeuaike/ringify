b58ed04e346bd3aa3d80303c63bfdc5d3e3d2eac
mikeuaike <mikeuaike@gmail.com>
2022-06-06 12:03:11 -0300

    pythonic rework

--- ringify.py
+++ ringify.py
@@ -35,29 +35,28 @@ z = z - z_com
 n = 25
 
 R = np.sqrt(x**2 + y**2)
-step = np.sqrt((x[np.argmax(x)]) ** 2 + (y[np.argmax(y)]) ** 2) / n
+step = np.sqrt(np.max(x) ** 2 + np.max(y) ** 2) / n
+
 
 # Initial Boundaries
 R1 = np.sqrt(x_com**2 + y_com**2)
 R2 = R1 + step
 
 print('Step: ', step, ' kpc')
-density_array = []
-theoric_array = []
-radius_array = []
+density_array = np.zeros(n)
+theoric_array = np.zeros(n)
+radius_array = np.zeros(n)
 
 print('Center: ', 'x = ', x_com, 'y = ', y_com)
 # Density Iteration
-i = 0
 for i in range(0, n):
     cond = np.argwhere((R > R1) & (R < R2)).flatten()
     sum_mass = np.sum(mass[cond])
     density = sum_mass / (np.pi*(R2 ** 2 - R1 ** 2))
-    density_array.append(density)
-    radius_array.append((R1 + R2) / 2)
+    density_array[i] = density
+    radius_array[i] = (R1 + R2) / 2
     print('ring ', i, ':', density, 'sun_mass 10^10 kpc^-2')
 
-    i = i + 1
 # move boundaries
     R1 = R2
     R2 = R2 + step
@@ -71,18 +70,19 @@ x_linspace = np.linspace(0, n*step, n)
 theor_density = ((m / (np.pi * (h ** 2))) * np.exp((-x_linspace) / h)) / 2
 
 # plot!
+label_0 = 'Densities from Snapshot'
+label_1 = 'Analytic Curve of Density'
 
-# x_axis = np.arange(n) * step
 x_axis = np.array(radius_array)
 y_axis = np.array(density_array)
 fig, ax = plt.subplots()
 
-ax.scatter(x_axis, y_axis, label='Densities from Snapshot', color='magenta')
-ax.plot(x_linspace, theor_density, label='Analytic Curve of Density', color='blue')
+ax.scatter(x_axis, y_axis, label=label_0, color='magenta')
+ax.plot(x_linspace, theor_density, label=label_1, color='b')
 
 plt.title("Density along Galactic Disk Radius")
-plt.xlabel("Radius [${\mathrm{kpc}}$]")
-plt.ylabel("Density [${\mathrm{M}}_{\odot}$ ${\mathrm{kpc}}^{-2}$]")
+plt.xlabel(r'Radius [${\mathrm{kpc}}$]')
+plt.ylabel(r'density [${\mathrm{M}}_{\odot}$ ${\mathrm{kpc}}^{-2}$]')
 plt.grid()
 plt.xscale("log")
 plt.yscale("log")
